cb33c09e1dba2daaee606d83621d4d97f668fe68
Caleb Smith <smithcaleb042@gmail.com>
2021-02-22 15:18:24 -0700

    testprep first commit

---
+++ testprep/mocktest.js
@@ -0,0 +1,105 @@
+/*
+What is the internet and how does it work?
+
+answer:
+
+
+What are some of the components of the physical network?
+
+answer:
+
+
+What is latency and bandwidth?
+
+answer:
+
+
+how do lower level protocals work? (high level abstraction).
+
+answer:
+
+
+What is an IP address and a port number?
+
+answer:
+
+
+How does DNS work?
+
+answer:
+
+
+how does the client-server model work in the context of web interactions?
+
+answer:
+
+
+what is TCP and UDP? How are they different?
+
+answer:
+
+
+What is the three way handshake and what is its purpose?
+
+answer:
+
+
+What is flow control and congestion avoidance?
+
+answer:
+
+
+What are the component of a URL?
+
+answer:
+
+
+construct a valid URL:
+
+answer:
+
+
+What is URL encoding and when is it used?
+
+answer:
+
+
+what are HTTP requests and responses and what are their components?
+
+answer:
+
+
+Describe the HTTP request/response cycle
+
+answer:
+
+
+What are status codes and describe the various types:
+
+answer:
+
+
+Describe 'state' in the context of the web. How do you simulate state?
+
+answer:
+
+
+what is the difference between GET and POST? 
+
+answer:
+
+
+What are the sucurity risks of HTTP? what can you do to mitigate those risks?
+
+answer:
+
+
+What are the differenct services that TLS provides?
+
+answer:
+
+
+
+
+
+*/
